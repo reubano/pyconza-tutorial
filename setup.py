f3fe392479cb31730aef5115368a0129534b4a90
Reuben Cummings <reubano@gmail.com>
2016-10-07 10:06:49 +0200

    Update requirements and setup

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ except ImportError:
 sys.dont_write_bytecode = True
 
 setup(
-    name='devcraft-workshop',
+    name='pyconza-tutorial',
     version='0.5.0',
-    description='devcraft-workshop materials',
+    description='pyconza-tutorial materials',
     author='Reuben Cummings',
     author_email='reubano@gmail.com',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
-    package_data={'data': ['*.csv']},
-    install_requires=['riko>=0.37.0,<0.38.0'],
+    package_data={'data': ['*.csv', '*.json', '*.xlsx']},
+    install_requires=['meza>=0.30.1,<0.31.0'],
     zip_safe=False
 )
